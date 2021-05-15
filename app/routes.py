6ac2a201e5dc64d580c340bc44f892336c332359
anders-m-c <christensen.anders945@gmail.com>
2021-05-16 00:43:48 +0800

    Updating routes, adding some content pages

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,11 @@ from app.controllers import UserController, QuizController, QuestionController
 import populate_database
 from sqlalchemy import and_
 
+from plotly.offline import plot
+from plotly.graph_objs import *
+from flask import Markup
+import plotly.express as px
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -49,9 +54,13 @@ def signup():
     return render_template('signup.html', title='Sign Up', form=form)
 
 
-@app.route('/learn', methods=['GET', 'POST'])
-def learn():
-    return render_template('learn.html', title='Learn')
+@app.route('/learn_beginner', methods=['GET', 'POST'])
+def learn_beginner():
+    return render_template('learn_beginner.html', title='Learn - Beginner')
+
+@app.route('/learn_intermediate', methods=['GET', 'POST'])
+def learn_intermediate():
+    return render_template('learn_intermediate.html', title='Learn - Intermediate')
 
 @app.route('/quiz/<int:id>', methods=['GET', 'POST'])
 def quiz(id):
@@ -64,6 +73,7 @@ def quiz(id):
     attemptNum = current_user.num_attempts
     #if it is not the start of a new quiz - get the user to their last saved question
     print("THIS IS THE ATTEMP NUM: ", attemptNum)
+    print("CURRENT_USER: ", current_user.sessionEnded)
     if current_user.sessionEnded == 0:#1 = THEY HAVE FINISHED 0 = THEY ARE IN A CURRENT SESSION
         print("FFS")
         #get the question number that the user was up to in their last session
@@ -92,25 +102,19 @@ def quiz(id):
     #get the next question
     q = Question.query.get(id)
     if not q:
+        current_user.sessionEnded = 1
+        db.session.commit()
         return redirect(url_for('results'))
-    #
-    if request.method == 'POST':
-        # quiz = Quiz.query.filter(Quiz.attemptNumber==attemptNum and Quiz.user_id==current_user.id).first()
-        
+    filelocation = "images/{}".format(q.img)
+    image_file = url_for('static', filename=filelocation)
+    if request.method == 'POST':        
         quiz = Quiz.query.filter(and_(Quiz.attemptNumber==attemptNum, Quiz.user_id==current_user.id)).first()
-        # quiz = quiz2
-        # for qui in quiz2:
-        #     if qui.author.username == current_user.username:
-        #         quiz = qui
         
         option = request.form['options']
         print(option)
         
-        #make an answer row with the answer etc.
         answer = Answer(questionNum=id, choice=option, author=quiz)
         db.session.add(answer)
-        
-        
         if option == q.correct_choice:
             quiz.result = quiz.result + 1
         quiz.questionNum = quiz.questionNum + 1
@@ -118,16 +122,105 @@ def quiz(id):
         db.session.commit()
         print("question num: ", id)
         return redirect(url_for('quiz', id=(id+1)))
-    return render_template('quiz3.html', q=q, title='Question {}'.format(id), id=id, numQues=numQuestions)
+    return render_template('quiz3.html', q=q, title='Question {}'.format(id), id=id, numQues=numQuestions, img=image_file)
 
 
 @app.route('/results', methods=['GET', 'POST'])
 @login_required
 def results():
     questions = Question.query.all()
-    return render_template('results.html', title='Results', questions=questions)
+    
+    plots = []
+    
+    choices = [0, 0, 0, 0]
+    
+    # how many choose choice 1... 
+    answers = Answer.query.all()
+    
+    for ques in questions:
+        for ans in answers:
+            if ans.questionNum == ques.id:
+                if ans.choice == ques.choice1:
+                    choices[0] = choices[0] + 1
+                elif ans.choice == ques.choice2:
+                    choices[1] = choices[1] + 1
+                elif ans.choice == ques.choice3:
+                    choices[2] = choices[2] + 1
+                elif ans.choice == ques.choice4:
+                    choices[3] = choices[3] + 1
+        names = [ques.choice1, ques.choice2, ques.choice3, ques.choice4]
+        fig = px.pie(values=choices, names=names, width=300, height=300)
+        fig.layout.update(showlegend=False)
+        div = fig.to_html(full_html=False)
+        plots.append(Markup(div))
+        choices = [0, 0, 0, 0]
+    
+    quiz = Quiz.query.filter(Quiz.attemptNumber==current_user.num_attempts).first()
+    print("QUIZZZ: ", quiz.id)
+    previous_quiz_answers = Answer.query.filter(Answer.quiz_id==quiz.id).all()
+    attempted_questions = len(previous_quiz_answers)
+    
+    return render_template('results.html', title='Results', questions=questions, div_placeholder=plots, previous_quiz_answers=previous_quiz_answers, attempted_questions=attempted_questions)
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+
+@app.route('/what_is_forex')
+def what_is_forex():
+    return render_template('content_pages/what_is_forex.html', title='What is Forex')
+
+@app.route('/buying_and_selling')
+def buying_and_selling():
+    return render_template('content_pages/buying_and_selling.html', title='Buying and Selling')
+
+@app.route('/how_to_make_money')
+def how_to_make_money():
+    return render_template('content_pages/how_to_make_money.html', title='How to Make Money')
+
+@app.route('/pips')
+def pips():
+    return render_template('content_pages/pips.html', title='Pips')
+
+@app.route('/lot_sizes')
+def lot_sizes():
+    return render_template('content_pages/lot_sizes.html', title='Lot Sizes')
+
+@app.route('/types_of_orders')
+def types_of_orders():
+    return render_template('content_pages/types_of_orders.html', title='Types of Orders')
+
+@app.route('/demo_vs_real')
+def demo_vs_real():
+    return render_template('content_pages/demo_vs_real.html', title='Demo Vs. Real')
+
+@app.route('/trading_sessions')
+def trading_sessions():
+    return render_template('content_pages/trading_sessions.html', title='Trading Sessions')
+
+@app.route('/candles')
+def candles():
+    return render_template('content_pages/candles.html', title='Candles')
+
+@app.route('/support_and_resistance')
+def support_and_resistance():
+    return render_template('content_pages/support_and_resistance.html', title='Support and Resistance')
+
+@app.route('/trends')
+def trends():
+    return render_template('content_pages/trends.html', title='Trends')
+
+@app.route('/trading_support_and_resistance')
+def trading_support_and_resistance():
+    return render_template('content_pages/trading_support_and_resistance.html', title='Trading Support and Resistance')
+
+@app.route('/candlestick_patterns')
+def candlestick_patterns():
+    return render_template('content_pages/candlestick_patterns.html', title='Candlestick Patterns')
+
+@app.route('/moving_averages')
+def moving_averages():
+    return render_template('content_pages/moving_averages.html', title='Moving Averages')
